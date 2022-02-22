9a25638b5d27c629e77b1b0da25b301ff14b7743
ethan-godwinn <55867179+ethan-godwinn@users.noreply.github.com>
2022-02-21 19:49:13 -0600

    Started on Stats toast

--- SourSalesmen/app/src/main/java/com/example/soursalesmen/MainActivity.kt
+++ SourSalesmen/app/src/main/java/com/example/soursalesmen/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.soursalesmen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     //set up variables
     private var lemonImage : ImageView? = null
-    private var scoreText : TextView? = null
+    public var scoreText : TextView? = null
     private var moneyString : String? = "$0"
-    private var clickCount = 0
+    public var clickCount = 0
+
+    private var statsButton : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,14 @@ class MainActivity : AppCompatActivity() {
 
         lemonImage!!.setOnClickListener {
             clickLemonImage()
+        }
+        statsButton = findViewById(R.id.statsButton)
+        //stats = findViewById(R.id.statsMenu)
+
+        //stats!!.visibility = View.INVISIBLE
+
+        statsButton!!.setOnClickListener{
+            openMenu()
         }
     }
 
@@ -37,5 +49,16 @@ class MainActivity : AppCompatActivity() {
         moneyString = "$ $clickCount"
 
         scoreText.text = moneyString
+    }
+
+    private fun openMenu() {
+        //stats!!.visibility = View.VISIBLE
+        val money : String = "$ $clickCount"
+        val duration = Toast.LENGTH_SHORT
+
+        val text : String = "Total money made: " + money
+
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
     }
 }
