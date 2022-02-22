97b4f9a56c886d65d382f751c68ce2eb52c715cd
westonwright <57042424+westonwright@users.noreply.github.com>
2022-02-21 21:48:24 -0600

    Upgradesss :)

--- SourSalesmen/app/src/main/java/com/example/soursalesmen/MainActivity.kt
+++ SourSalesmen/app/src/main/java/com/example/soursalesmen/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.soursalesmen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.os.CountDownTimer
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,21 +11,27 @@ class MainActivity : AppCompatActivity() {
     private var lemonImage : ImageView? = null
     public var scoreText : TextView? = null
     private var moneyString : String? = "$0"
-    public var clickCount = 0
+    private var clickCount = 0
+    private var clickMultiplier = 1
+    private var valueMultiplier = 1
+
+    private var timerCooldown : Long = 1000
+    private var idleButton : Button? = null
+    private var idleLemons = false
+    private var clickButton : Button? = null
+    private var clickMultiplied = false
+    private var valueButton : Button? = null
+    private var valueMultiplied = false
 
     private var statsButton : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         lemonImage = findViewById(R.id.lemonImage)
         scoreText = findViewById(R.id.scoreText)
         moneyString = resources.getString(R.string.money)
 
-        lemonImage!!.setOnClickListener {
-            clickLemonImage()
-        }
         statsButton = findViewById(R.id.statsButton)
         //stats = findViewById(R.id.statsMenu)
 
@@ -36,10 +40,66 @@ class MainActivity : AppCompatActivity() {
         statsButton!!.setOnClickListener{
             openMenu()
         }
+
+        idleButton = findViewById(R.id.button1)
+        idleButton!!.setOnClickListener{
+            clickButton1()
+        }
+        clickButton = findViewById((R.id.button2))
+        clickButton!!.setOnClickListener{
+            clickButton2()
+        }
+        valueButton = findViewById((R.id.button3))
+        valueButton!!.setOnClickListener{
+            clickButton3()
+        }
+
+        lemonImage!!.setOnClickListener {
+            clickLemonImage()
+        }
+
     }
 
+    fun initCountDownTimer(time: Long) {
+        object : CountDownTimer(time, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                idleClick()
+                initCountDownTimer(timerCooldown)
+            }
+        }.start()
+    }
+
+    private fun clickButton1(){
+        if(!idleLemons){
+            initCountDownTimer(timerCooldown)
+            idleLemons = true
+        }
+    }
+    private  fun clickButton2(){
+        if(!clickMultiplied){
+            clickMultiplier += 2
+            clickMultiplied = true
+
+        }
+    }
+
+    private  fun clickButton3(){
+        if(!valueMultiplied){
+            valueMultiplier += 1
+            valueMultiplied = true
+        }
+    }
+
+    private fun idleClick(){
+        clickCount+=valueMultiplier
+
+        updateView()
+    }
     private fun clickLemonImage() {
-        clickCount++
+        clickCount+=clickMultiplier * valueMultiplier
 
         updateView()
     }
