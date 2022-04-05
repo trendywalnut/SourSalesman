bff05bef02e381800dc26de0aadafd047dbbffe1
ethan-godwinn <55867179+ethan-godwinn@users.noreply.github.com>
2022-04-04 19:03:42 -0500

    Stats window opens and closes

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.example.quizapp
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         title = "Quizdle"
 
+        val statsLayout:RelativeLayout = findViewById(R.id.statsLayout)
+        val closeButton:ImageButton = findViewById(R.id.closeStatButton)
+        //val statsLayout:RelativeLayout = findViewById(R.id.statsLayout)
+        statsLayout.setVisibility(View.GONE)
+
         val button:Button = findViewById(R.id.questionButton)
         button.setOnClickListener{
             val intent = Intent(this@MainActivity, Question1::class.java)
             startActivity(intent)
         }
+
+        val statButton:Button = findViewById(R.id.statsButton)
+        statButton.setOnClickListener{
+            statsLayout.setVisibility(View.VISIBLE)
+        }
+
+        closeButton.setOnClickListener{
+            statsLayout.setVisibility(View.GONE)
+        }
     }
+
+
 }
